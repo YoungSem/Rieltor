89cdb6ec56ddf93cec79a1ebe61891a59e09d656
Georgy <dmitrichenkog@vk.com>
2025-05-22 15:56:51 +0500

    Правка в подключении static

--- realtor_site/main/admin.py
+++ realtor_site/main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Jk
+
+@admin.register(Jk)
+class JkAdmin(admin.ModelAdmin):
+    list_display = ('name', 'area', 'description')
+    search_fields = ('name', 'area')
+    list_filter = ('area',)
