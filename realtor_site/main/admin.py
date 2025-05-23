efdfa2e44e54de5b81c76409078ca9c3e4170079
Georgy <dmitrichenkog@vk.com>
2025-05-23 14:26:56 +0500

    Подготовка к слиянию. Удалил код для тестирования

--- realtor_site/main/admin.py
+++ realtor_site/main/admin.py
@@ -1,9 +1,3 @@
 from django.contrib import admin
 
-from .models import Jk
 
-@admin.register(Jk)
-class JkAdmin(admin.ModelAdmin):
-    list_display = ('name', 'area', 'description')
-    search_fields = ('name', 'area')
-    list_filter = ('area',)
